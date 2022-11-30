dd8c2fd839e762625019f5cd01daadcb9c70da1e
Slav Konovalov <slav.konovalov@gmail.com>
2022-11-30 22:13:53 +0000

    implemented uploading of new recipe to API + added gitignore

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 3;
 export const RES_PER_PAGE = 10;
+export const KEY = '80bc9377-2bb0-4f50-9db7-1e5809541853';
+export const MODAL_CLOSE_SEC = 2.5;
